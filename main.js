e22448b86dc388350c4f1e895355542b149224ec
solexy79 <agboolasola6@gmail.com>
2020-06-18 10:27:00 +0100

    init commit

---
+++ main.js
@@ -0,0 +1,6 @@
+document.querySelector('#darkMode').addEventListener('click', () => {
+  // document.querySelector('.social-grid').classList.toggle('change');
+  document.querySelector('.container').classList.toggle('change');
+  document.querySelector('.container-wrapper').classList.toggle('change');
+  
+})
